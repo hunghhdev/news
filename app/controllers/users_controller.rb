af0686e16dae07c39d967917e4ea138bc6f62212
hungmast3r <hotro.giaitri23@gmail.com>
2018-03-08 10:14:10 +0700

    chap 10: updating and deleting users (#10)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   attr_reader :user
 
+  before_action :logged_in_user, :find_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
+
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.page.minimum
+  end
+
   def new
     @user = User.new
   end
@@ -17,7 +25,28 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  def update
+    if user.update_attributes user_params
+      flash[:success] = t ".profileupdate"
+      redirect_to user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user.destroy
+    flash[:success] = t ".deleted"
+    redirect_to users_url
+  end
+
+  private
+
+  def find_user
     @user = User.find_by id: params[:id]
 
     return if user
@@ -25,10 +54,23 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
-  private
-
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "users.edit.pleaselogin"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless user.current_user? current_user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
